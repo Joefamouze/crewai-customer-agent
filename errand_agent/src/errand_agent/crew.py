a7e6e3df91f00a601276613a7ec0ba619d9f483a
Jonathan Enudeme <twoejoe5@gmail.com>
2025-07-30 17:56:37 +0100

    added knowledge and modified config yaml files

--- errand_agent/src/errand_agent/crew.py
+++ errand_agent/src/errand_agent/crew.py
@@ -1,29 +1,41 @@
+import os
+import dotenv
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai.knowledge.source.text_file_knowledge_source import TextFileKnowledgeSource
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from langchain_openai import ChatOpenAI
 from typing import List
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+if "OPENAI_API_KEY" not in os.environ:
+    print("Error: OPENAI_API_KEY environment variable not set.")
+    exit()
+
+llm = ChatOpenAI(model="gpt-4.1")
+
+text_source = TextFileKnowledgeSource(
+    file_paths=["user_preference.txt"]
+)
 
 @CrewBase
 class ErrandAgent():
     """ErrandAgent crew"""
 
-    agents: List[BaseAgent]
-    tasks: List[Task]
+    agents_config = "config/agents.yaml"
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def researcher(self) -> Agent:
+    def information_extractor(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
+            config=self.agents_config['information_extractor'],
+            verbose=True,
+            knowledge_sources=["user_preferences.txt"],
+            memory=True
         )
 
     @agent
